6f64dd6c0cdacd050fe61d9387ee75a5912c7250
Brett Terpstra <brett.terpstra@oracle.com>
2024-12-10 12:03:01 -0600

    Adding is.gd shortener for Setapp links

--- bitly_setapp.rb
+++ bitly_setapp.rb
@@ -76,14 +76,18 @@ module SL
 
         domain = SL.config.key?('bitly_domain') ? SL.config['bitly_domain'] : 'bit.ly'
         long_url = url.dup
-        curl = TTY::Which.which('curl')
-        cmd = [
-          %(#{curl} -SsL -H 'Authorization: Bearer #{SL.config['bitly_access_token']}'),
-          %(-H 'Content-Type: application/json'),
-          '-X POST', %(-d '{ "long_url": "#{url}", "domain": "#{domain}" }'), 'https://api-ssl.bitly.com/v4/shorten'
-        ]
-        data = JSON.parse(`#{cmd.join(' ')}`.strip)
-        link = data['link']
+
+        headers = {
+          'Content-Type' => 'application/json',
+          'Authorization' => "Bearer #{SL.config['bitly_access_token']}"
+        }
+        data_obj = {
+          'long_url' => url,
+          'domain' => domain
+        }
+        data = Curl::Json.new('https://api-ssl.bitly.com/v4/shorten', data: data_obj.to_json, headers: headers, symbolize_names: true)
+
+        link = data.json[:link]
         rtitle = SL::URL.title(long_url)
         title = rtitle
         link_text = rtitle if link_text == '' && !SL.titleize
